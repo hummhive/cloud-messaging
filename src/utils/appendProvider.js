3f5b7a3388b4e821510b4f2c5126797010fbfc00
Pedro <pedro@fusionbreak.com>
2022-07-10 21:28:27 -0400

    feat: Moves emailTemplate into the publisher and append newsletter unsubscribe

---
+++ src/utils/appendProvider.js
@@ -0,0 +1,5 @@
+const appendProvider = (content) => {
+  return content.concat(`<div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="color:#444444; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5"><div class="Unsubscribe--addressLine"><p class="Unsubscribe--senderName" style="font-size:12px; line-height:20px;">© 2022 erer</p></div><p style="font-size:12px; line-height:20px;"><a class="Unsubscribe--unsubscribeLink" href="{{{unsubscribe}}}" target="_blank" style="">Unsubscribe</a> - <a href="{{{unsubscribe_preferences}}}" target="_blank" class="Unsubscribe--unsubscribePreferences" style="">Unsubscribe Preferences</a></p></div>`)
+};
+
+export default appendProvider;
